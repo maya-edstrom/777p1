f48e65d2624c2739adfcf5a8b12d73f88e127901
maya-edstrom <medstrom@wisc.edu>
2024-06-24 21:06:06 -0500

    updating

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import arcpy 
 import os, sys
 
+import arcpy.geoprocessing
+
 arcpy.env.overwriteOutput = True # Allow the overwriting of the output files
 
 try:
@@ -30,15 +32,13 @@ except:
 
 #step 2: interpolating nitrate values to census blocks using IDW tool using different user inputs for k value
 
-    k = 2 #will need to get user input eventually
-        
+    power = 2 #will need to get user input eventually
     inPoint = "well.nitrate.shp" # Set the input point feature class to the well nitrate shapefile
     zField = "nitr_ran"
     outLayer = "IDW" # Set the output layer to IDW
     
     # Execute IDW
-    outIDW = Idw(inPointFeatures, zField, cellSize, power, searchRadius)
-
+    outIDW = arcpy.geoprocessing.IDW(inPointFeatures, zField, cellSize, power, searchRadius)
     # Save the output 
     outIDW.save("C:/sapyexamples/output/idwout02")
 
