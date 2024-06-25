56c87bcbbd9f9c877180823b989a16a4273d7d23
maya-edstrom <medstrom@wisc.edu>
2024-06-24 21:18:26 -0500

    edits

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ env.workspace = "C:\777p1\data" # Set the workspace
   
 #step 1: interpolating nitrate values to census blocks using IDW tool using different user inputs for k value
 
-    power = 2 #will need to get user input eventually
-    inPoint = "well.nitrate.shp" # Set the input point feature class to the well nitrate shapefile
-    zField = "nitr_ran"
-    outLayer = "IDW" # Set the output layer to IDW
+power = 2 #will need to get user input eventually
+inPoint = "well.nitrate.shp" # Set the input point feature class to the well nitrate shapefile
+zField = "nitr_ran"
+outLayer = "IDW" # Set the output layer to IDW
     
-    # Execute IDW
-    outIDW = arcpy.sa.Idw(inPoint, zField, power) # Perform the IDW interpolation using the input point feature class, the field to interpolate, and the power value
-    # Save the output 
-    outIDW.save("C:/sapyexamples/output/idwout02")
+# Execute IDW
+outIDW = arcpy.sa.Idw(inPoint, zField, power) # Perform the IDW interpolation using the input point feature class, the field to interpolate, and the power value
+# Save the output 
+outIDW.save("C:/sapyexamples/output/idwout02")
 
-    print("IDW interpolation complete") # Print that the interpolation is complete
+print("IDW interpolation complete") # Print that the interpolation is complete
 
