241f6fceb5b1691842e4f20d4b9b58b7240c84f4
maya-edstrom <medstrom@wisc.edu>
2024-06-24 20:09:15 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import arcpy 
+import os, sys
+
+arcpy.env.overwriteOutput = True # Allow the overwriting of the output files
+
+try:
+    arcpy.env.workspace = "C:\777p1\data" # Set the workspace
+except:
+    print('Failed to establish workspace')
+    sys.exit()
+
+#step 1: aggregating/preparing the data
+try: 
+    cancer_tracts = "cancer.tracts.shp" # Open the cancer tracts shapefile and store in variable
+except:
+    print('Failed to open shapefile')
+    sys.exit()
+
+try: 
+    well_nitrate = "well.nitrate.shp" # Open the well nitrate shapefile and store in variable
+except:
+    print('Failed to open shapefile')
+    sys.exit()
+
+try:
+    census_blocks = "WI_CensusTL_BlockGroups_2020.shp" # Open the census blocks shapefile and store in variable
+
+except: 
+    print('Failed to open shapefile')
+    sys.exit()
