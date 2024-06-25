2ae0a59e16d8794424b19c6680fddd244b4917be
maya-edstrom <medstrom@wisc.edu>
2024-06-24 20:48:12 -0500

    got rid of sys.exits for now

--- main.py
+++ main.py
@@ -7,27 +7,26 @@ try:
     arcpy.env.workspace = "C:\777p1\data" # Set the workspace
 except:
     print('Failed to establish workspace')
-    sys.exit()
+  
 
 #step 1: saving shapefiles to variables
 try: 
     cancer_tracts = "cancer.tracts.shp" # Open the cancer tracts shapefile and store in variable
 except:
     print('Failed to open shapefile')
-    sys.exit()
+
 
 try: 
     well_nitrate = "well.nitrate.shp" # Open the well nitrate shapefile and store in variable
 except:
     print('Failed to open shapefile')
-    sys.exit()
-
+   
 try:
     census_blocks = "WI_CensusTL_BlockGroups_2020.shp" # Open the census blocks shapefile and store in variable
 
 except: 
     print('Failed to open shapefile')
-    sys.exit()
+ 
 
 #step 2: interpolating nitrate values to census blocks using IDW tool using different user inputs for k value
 
