cb99e7c5e827d2a1707d4ec65872aa93ca03e4ba
maya-edstrom <122655531+maya-edstrom@users.noreply.github.com>
2024-09-16 15:14:38 -0500

    basic tkinter script

--- tkinter.py
+++ tkinter.py
@@ -1,2 +1,12 @@
 from tkinter import *
 
+root = Tk()
+
+#creating a label widget
+pgTitle = Label(root, text="Cancer Rates vs. Nitrate Concentrations")
+
+#shoving it onto the screen
+pgTitle.pack()
+
+#create event loop
+root.mainLoop() 
