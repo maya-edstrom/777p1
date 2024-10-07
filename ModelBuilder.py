0d5d20bfa721743e8f83d96414a6b7950c9548e1
maya-edstrom <122655531+maya-edstrom@users.noreply.github.com>
2024-10-07 14:38:17 -0500

    fixed formatting issue

--- ModelBuilder.py
+++ ModelBuilder.py
@@ -26,13 +26,10 @@ def Model():  # Model
     nitrate_IDW = arcpy.sa.Idw(well_nitrate_OG, "nitr_ran", "0.017616319278512", 2, "VARIABLE 12", "")
     nitrate_IDW.save(IDW)
 
-
     # Process: Zonal Statistics as Table (Zonal Statistics as Table) (sa)
     CancerZonalStats = "C:\\777p1\\CancerZonalStats"
     Output_Join_Layer = ""
     arcpy.sa.ZonalStatisticsAsTable(cancer_tracts_OG, "GEOID10", nitrate_IDW, CancerZonalStats, "DATA", "ALL", "CURRENT_SLICE", [90], "AUTO_DETECT", "ARITHMETIC", 360, Output_Join_Layer)
-    .save(Zonal_Statistics_as_Table)
-
 
     # Process: Add Join (Add Join) (management)
     cancer_tracts_OG_2_ = arcpy.management.AddJoin(in_layer_or_view=cancer_tracts_OG_3_, in_field="GEOID10", join_table=CancerZonalStats, join_field="GEOID10")[0]
