c668745cf1118ee70e2bc267b6752c53101650cf
maya-edstrom <122655531+maya-edstrom@users.noreply.github.com>
2024-10-08 21:39:03 -0500

    add pdf export code

--- exportMapPDF.py
+++ exportMapPDF.py
@@ -9,3 +9,5 @@ m = aprx.listMaps("Map")[0]
 lyrFile = arcpy.mp.LayerFile(r"C:\777p1\ArcGISPro\Geog777_P1\OLS_fc_SaveToLayerFile.lyrx")
 m.addLayer(lyrFile, "TOP")
 
+lyt = aprx.listLayouts("Layout")[0]
+lyt.exportToPDF(r"C:\777p1\OLS_Map.pdf")
